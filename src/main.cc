1864dc3f5d1160f202fd119ffbe89255d7343e03
mairon <3043462073@qq.com>
2024-07-30 19:56:39 +0800

    ui: version display

--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
     Logger logger(Logger::Level::debug, "logs/evento.log");
     spdlog::info("SAST Evento version: v" VERSION_FULL);
     auto ui = App::create();
+    ui->set_version_string("v" VERSION_FULL);
     ui->on_loginClicked(start_sast_link);
     ui->run();
 }
