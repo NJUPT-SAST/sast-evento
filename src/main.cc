f04891eab7f91733affaa4b2eea9a8c368b01bd6
Mairon <3043462073@qq.com>
2024-07-30 22:55:15 +0800

    feat: log to system temp dir

--- src/main.cc
+++ src/main.cc
@@ -2,10 +2,12 @@
 #include <Infrastructure/Utils/Logger.h>
 #include <Version.h>
 #include <app.h>
+#include <filesystem>
 #include <spdlog/spdlog.h>
 
 int main(int argc, char** argv) {
-    Logger logger(Logger::Level::debug, "logs/evento.log");
+    Logger logger(Logger::Level::debug,
+                  std::filesystem::temp_directory_path() / "NJUPT-SAST" / "logs" / "evento.log");
     spdlog::info("SAST Evento version: v" VERSION_FULL);
     auto ui = App::create();
     ui->set_version_string("v" VERSION_FULL);
