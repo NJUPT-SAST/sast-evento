bcf3e9a1356157d9b23f20ac849f6f0843badd01
cEvolve05 <cEvolve05@gmail.com>
2024-08-16 22:59:55 +0800

    feat(ui): modify overlay, improve menu and login

--- src/Controller/Core/MessageManager.cc
+++ src/Controller/Core/MessageManager.cc
@@ -16,7 +16,6 @@ MessageManager::MessageManager(slint::ComponentHandle<UiEntryName> uiEntry, UiBr
     auto& self = *this;
 
     self->on_show_message([this](slint::SharedString content, MessageType type) {
-        spdlog::debug("pre new message");
         showMessage(std::string(content), type);
     });
 }
@@ -25,13 +24,14 @@ void MessageManager::showMessage(std::string content,
                                  MessageType type,
                                  std::chrono::milliseconds timeout) {
     auto& self = *this;
+
     UiUtility::StylishLog::newMessageShowed(logOrigin, content);
-    spdlog::debug("new message");
 
     self->set_type(type);
     self->set_content(std::string_view(content));
     self->set_timeout(timeout.count());
     self->set_visible(true);
+
     // TODO: find a better way to delay
     std::thread t([&self] {
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
