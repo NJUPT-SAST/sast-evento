3e71080e6aba758d379ac436288ad39b1bca10c3
DTPA101 <154043471+DTPA101@users.noreply.github.com>
2024-08-22 20:41:25 +0800

    feat(ui): finish setting page (#21)
    * feat:some basic laying
    
    * feat(ui): basic laying on the page
    
    * ui laying primary draft
    
    * feat(ui): callbacks and root
    
    * attempt to fill in .cc and .slint
    
    * callbacks fiied
    
    * feat: setting page
    
    * feat: change theme when app start
    
    * chore: clean up code
    
    * feat(ui): add scroll view in setting page
    
    ---------
    
    Co-authored-by: Serein <2075337935@qq.com>

--- src/Controller/View/SettingPage.cc
+++ src/Controller/View/SettingPage.cc
@@ -1,13 +1,67 @@
 #include <Controller/View/SettingPage.h>
+#include <Infrastructure/Utils/Config.h>
 
 EVENTO_UI_START
 
 SettingPage::SettingPage(slint::ComponentHandle<UiEntryName> uiEntry, UiBridge& bridge)
     : BasicView(bridge)
-    , GlobalAgent(uiEntry) {}
+    , GlobalAgent(uiEntry) {
+    auto& self = *this;
+
+    const auto [languageIdx, minimalToTray, noticeBegin, noticeEnd, themeIdx] = evento::settings;
+
+    self->set_language_index(languageIdx);
+    self->set_minimal_to_tray(minimalToTray);
+    self->set_notice_begin(noticeBegin);
+    self->set_notice_end(noticeEnd);
+    self->set_theme_index(themeIdx);
+
+    self->invoke_change_theme();
+}
 
 void SettingPage::onCreate() {
     auto& self = *this;
+
+    const auto [languageIdx, minimalToTray, noticeBegin, noticeEnd, themeIdx] = evento::settings;
+
+    config.insert_or_assign("setting",
+                            toml::table{
+                                {"language", languageIdx},
+                                {"minimal-to-tray", minimalToTray},
+                                {"notice-begin", noticeBegin},
+                                {"notice-end", noticeEnd},
+                                {"theme", themeIdx},
+                            });
+
+    self->on_language_changed([this]() {
+        auto& self = *this;
+        auto& setting = config["setting"].ref<toml::table>();
+        setting.insert_or_assign("language", self->get_language_index());
+    });
+
+    self->on_minimal_to_tray_changed([this]() {
+        auto& self = *this;
+        auto& setting = config["setting"].ref<toml::table>();
+        setting.insert_or_assign("minimal-to-tray", self->get_minimal_to_tray());
+    });
+
+    self->on_notice_begin_changed([this]() {
+        auto& self = *this;
+        auto& setting = config["setting"].ref<toml::table>();
+        setting.insert_or_assign("notice-begin", self->get_notice_begin());
+    });
+
+    self->on_notice_end_changed([this]() {
+        auto& self = *this;
+        auto& setting = config["setting"].ref<toml::table>();
+        setting.insert_or_assign("notice-end", self->get_notice_end());
+    });
+
+    self->on_theme_changed([this]() {
+        auto& self = *this;
+        auto& setting = config["setting"].ref<toml::table>();
+        setting.insert_or_assign("theme", self->get_theme_index());
+    });
 }
 
 EVENTO_UI_END
