716328e3806dc0600fcafc9f4843d90be602d926
zwow-cj <2804818334@qq.com>
2024-09-19 21:07:04 +0800

    first logic

--- src/Controller/View/MyEventPage.cc
+++ src/Controller/View/MyEventPage.cc
@@ -1,6 +1,31 @@
+#include "Controller/AsyncExecutor.hh"
+#include "Infrastructure/Network/NetworkClient.h"
+#include "Infrastructure/Utils/Result.h"
+#include "slint.h"
+#include "slint_string.h"
 #include <Controller/View/MyEventPage.h>
+#include <Infrastructure/Network/ResponseStruct.h>
+#include <vector>
 
 EVENTO_UI_START
+
+enum InfoPart{
+    sum = 1,
+    description = 2,
+    start = 3,
+    end = 4,
+    location = 5,
+    tag = 6,
+    larkMeetingRoomName = 7,
+    larkDepartmentName = 8,
+    state = 9,
+    isSubscribed = 10,
+    isCheckedIn = 11
+};
+
+evento::EventEntityList lists_sub = MyEventPage::get_events(slint::SharedString("subscriptions"));
+evento::EventEntityList lists_end = MyEventPage::get_events(slint::SharedString("end"));
+evento::EventQueryRes res_active = MyEventPage::get_events();
 
 MyEventPage::MyEventPage(slint::ComponentHandle<UiEntryName> uiEntry, UiBridge& bridge)
     : BasicView(bridge)
@@ -8,6 +33,78 @@ MyEventPage::MyEventPage(slint::ComponentHandle<UiEntryName> uiEntry, UiBridge& 
 
 void MyEventPage::onCreate() {
     auto& self = *this;
+    
+}
+void MyEventPage::onShow() {
+    auto& self = *this;
+    // show event when the page is shown
+    
+    //if click button , check code
+    
+};
+
+evento::EventEntityList MyEventPage::get_events(slint::SharedString part) {
+    evento::EventEntityList eventList;
+    if (part == slint::SharedString("subscriptions")) {
+        evento::executor()->asyncExecute(networkClient()->getSubscribedEvent(), [&](Result<EventEntityList> list){
+        if (list.isOk()){
+            eventList = list.unwrap();
+        }else {
+            return ;
+        }
+    });
+    }
+    else if (part == slint::SharedString("end")) {
+        evento::executor()->asyncExecute(networkClient()->getParticipatedEvent(), [&](Result<EventEntityList> list){
+        if (list.isOk()){
+            eventList = list.unwrap();
+        }else {
+            return ;
+        }
+    });
+    }
+    return eventList;
+}
+
+evento::EventQueryRes MyEventPage::get_events(){
+    evento::EventQueryRes eventlists;
+    evento::executor()->asyncExecute(networkClient()->getActiveEventList(), [&](Result<evento::EventQueryRes> list){
+        if (list.isOk()){
+            eventlists = list.unwrap();
+        }else {
+            return ;
+        }
+    });
+    return eventlists;
+}
+
+std::vector<slint::SharedString> MyEventPage::process_events(std::string& part) {
+    std::vector<slint::SharedString> result;
+    auto size1 = lists_sub.size();
+    auto size2 = lists_end.size();
+    auto size3 = res_active.elements.size();
+    if (part == "subscriptions") {
+
+    }else if (part == "end") {
+    
+    }else if (part == "active") {
+    
+    }
+    return result;
+}
+
+// ToDo implement this function about eventIde
+bool MyEventPage::check_code(slint::SharedString code, int id) {
+    bool is_check_in = false;
+    std::string the_code = std::string(code);
+    evento::executor()->asyncExecute(evento::networkClient()->checkInEvent(id, the_code), [&](Result<bool> check){
+        if (check.isOk()) {
+            is_check_in = check.unwrap();
+        } else if (check.isErr()) {
+            return ;
+        }
+    });
+    return is_check_in;
 }
 
 EVENTO_UI_END
--- src/Controller/View/MyEventPage.h
+++ src/Controller/View/MyEventPage.h
@@ -1,17 +1,29 @@
 #pragma once
 
+#include "Infrastructure/Network/NetworkClient.h"
+#include "slint_string.h"
 #include <Controller/Core/BasicView.h>
 #include <Controller/Core/GlobalAgent.hh>
 #include <Controller/Core/UiBase.h>
+#include <Infrastructure/Network/ResponseStruct.h>
+#include <vector>
 
 EVENTO_UI_START
 
 class MyEventPage : public BasicView, private GlobalAgent<MyEventPageBridge> {
 public:
     MyEventPage(slint::ComponentHandle<UiEntryName> uiEntry, UiBridge& bridge);
-
+    //get info
+    static evento::EventEntityList get_events(slint::SharedString part);
+    static evento::EventQueryRes get_events();
 private:
     void onCreate() override;
+    void onShow() override;
+
+    static bool check_code(slint::SharedString code, int id);
+    //process info
+    static std::vector<slint::SharedString> process_events(std::string& part);
+    static std::vector<slint::SharedString> process_events(const evento::EventEntityList& events, std::string& part);
 };
 
 EVENTO_UI_END
