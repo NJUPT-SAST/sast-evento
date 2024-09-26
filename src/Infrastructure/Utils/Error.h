bbbc1a310cb19a7c37c66e0cbd88be59ef9b2e19
Wyatt Wong <635118061@qq.com>
2024-09-26 19:17:10 +0800

    feat(network): reflect Errorcode to specific ErrorMessage

--- src/Infrastructure/Utils/Error.h
+++ src/Infrastructure/Utils/Error.h
@@ -1,18 +1,14 @@
 // IWYU pragma: private, include <Infrastructure/Utils/Result.h>
 #pragma once
+
 #include <string>
 #include <unordered_map>
+
 namespace evento {
 
 class Error {
 public:
-    enum Kind {
-        Ssl = 0,
-        Network,
-        JsonDes,
-        Data,
-        Unknown,
-    } kind;
+    enum Kind { Ssl = 0, Network, JsonDes, Data, Unknown, Timeout } kind;
 
     Error(Kind kind, std::string_view reason)
         : kind(kind)
@@ -22,6 +18,10 @@ public:
         : kind(kind)
         , _reason(_reasonMap[kind]) {}
 
+    Error(int code)
+        : kind(Unknown)
+        , _reason(_errorCodeMap.at(code)) {}
+
     [[nodiscard]] std::string what() const { return _reason; }
 
     operator std::string() const { return what(); }
@@ -29,22 +29,24 @@ public:
 private:
     std::string _reason;
 
-    inline static std::string _reasonMap[Unknown + 2] = {"SSL error!",
+    inline static std::string _reasonMap[Timeout + 1] = {"SSL error!",
                                                          "Network error!",
                                                          "Json Deserialization error!",
                                                          "Data error",
-                                                         "Timeout error!",
-                                                         "Unknown Error"};
-    inline static std::unordered_map<int, std::string> _errorCodeMap = {{100, "Continue"},
-                                                                        {200, "OK"},
-                                                                        {400, "Bad Request"},
-                                                                        {401, "Unauthorized"},
-                                                                        {403, "Forbidden"},
-                                                                        {404, "Not Found"},
-                                                                        {500,
-                                                                         "Internal Server Error"},
-                                                                        {502, "Bad Gateway"},
-                                                                        {503,
-                                                                         "Service Unavailable"}};
+                                                         "Unknown Error",
+                                                         "Timeout error!"};
+
+    inline static std::unordered_map<int, std::string> _errorCodeMap = {
+        {100, "Continue"},
+        {200, "OK"},
+        {400, "Bad Request"},
+        {401, "Unauthorized"},
+        {403, "Forbidden"},
+        {404, "Not Found"},
+        {500, "Internal Server Error"},
+        {502, "Bad Gateway"},
+        {503, "Service Unavailable"},
+    };
+};
 
 } // namespace evento
