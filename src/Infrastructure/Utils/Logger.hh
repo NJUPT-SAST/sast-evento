7e56841eceffe87ecc0fdd2abd49f2bfa6000b5a
Serein <2075337935@qq.com>
2024-10-02 00:01:34 +0800

    feat(logger): update logger initialization and configuration

--- src/Infrastructure/Utils/Logger.hh
+++ src/Infrastructure/Utils/Logger.hh
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <spdlog/async.h>
+#include <spdlog/async_logger.h>
 #include <spdlog/details/thread_pool.h>
 #include <spdlog/sinks/daily_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 class Logger {
+    using LoggerType = spdlog::logger;
+
 public:
     using Level = spdlog::level::level_enum;
     Logger(Level level,
@@ -20,38 +23,45 @@ public:
         handlers.before_close = [](spdlog::filename_t filename, std::FILE* fstream) {
             fputs("[End SAST-Evento-Desktop]\n\n\n\n\n", fstream);
         };
-        std::shared_ptr<spdlog::sinks::daily_file_sink_mt> fileSink
-            = std::make_shared<spdlog::sinks::daily_file_sink_mt>(filepath, 0, 0, false, 0, handlers);
-        std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> stdoutSink
-            = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        _threadPool = std::make_shared<spdlog::details::thread_pool>(q_max_items, thread_count);
-        _asyncLogger = std::make_shared<spdlog::async_logger>("evento",
-                                                              spdlog::sinks_init_list{fileSink,
-                                                                                      stdoutSink},
-                                                              _threadPool);
+        auto fileSink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(filepath,
+                                                                            0,
+                                                                            0,
+                                                                            false,
+                                                                            0,
+                                                                            handlers);
+        auto stdoutSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        if constexpr (std::is_same_v<LoggerType, spdlog::async_logger>) {
+            _threadPool = std::make_shared<spdlog::details::thread_pool>(q_max_items, thread_count);
+            _logger = std::make_shared<LoggerType>("evento",
+                                                   spdlog::sinks_init_list{fileSink, stdoutSink},
+                                                   _threadPool);
+        } else {
+            _logger = std::make_shared<LoggerType>("evento",
+                                                   spdlog::sinks_init_list{fileSink, stdoutSink});
+        }
 
 #ifdef EVENTO_RELEASE
         stdoutSink->set_level(Level::off);
 #endif
-        _asyncLogger->set_level(level);
-        _asyncLogger->set_error_handler(
-            [&](const std::string& msg) { _asyncLogger->error("*** LOGGER ERROR ***: {}", msg); });
+        _logger->set_level(level);
+        _logger->set_error_handler(
+            [&](const std::string& msg) { _logger->error("*** LOGGER ERROR ***: {}", msg); });
         // spdlog::register_logger(_asyncLogger);
 
         /* more about pattern:
          * https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
          */
         stdoutSink->set_pattern("\033[36m[%Y-%m-%d %H:%M:%S.%e] \033[92m[%n] \033[0m%^[%l]%$ %v");
-        spdlog::set_default_logger(_asyncLogger);
+        spdlog::set_default_logger(_logger);
     }
-    auto& logger() { return _asyncLogger; }
-    [[nodiscard]] Level logLevel() const { return _asyncLogger->level(); }
-    void setLogLevel(Level level) { _asyncLogger->set_level(level); }
+    auto& logger() { return _logger; }
+    [[nodiscard]] Level logLevel() const { return _logger->level(); }
+    void setLogLevel(Level level) { _logger->set_level(level); }
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
     ~Logger() { spdlog::shutdown(); }
 
 private:
-    std::shared_ptr<spdlog::async_logger> _asyncLogger;
+    std::shared_ptr<LoggerType> _logger;
     std::shared_ptr<spdlog::details::thread_pool> _threadPool;
 };
